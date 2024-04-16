c79d0565bfad1d9856af39a59bad13c028b04b6a
MaryanKostrubyak <160321397+MaryanKostrubyak@users.noreply.github.com>
2024-04-17 00:46:36 +0300

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -3,14 +3,14 @@ function triangle(num1, type1, num2, type2) {
     let a, b, c, alpha, beta;
     
     if (num1 <= 0 || num2 <= 0) {
-        return "failed";
+        return "failed: one or both of the numbers is less than or equal to zero";
     }
     else if (type1 === "leg" && type2 === "leg") {
         a = num1;
         b = num2;
-        c = Math.sqrt(a**2+b**2);
-        alpha = Math.asin(a / c) * (180 / Math.PI);
-        beta = Math.asin(b / c) * (180 / Math.PI);
+        c = Math.sqrt(a**2 + b**2);
+        alpha = Math.atan(a / b) * (180 / Math.PI);
+        beta = 90 - alpha;
     } else if (type1 === "leg" && type2 === "hypotenuse") {
         a = num1;
         c = num2;
@@ -28,13 +28,13 @@ function triangle(num1, type1, num2, type2) {
         beta = num2;
         alpha = 90 - beta;
         c = a / Math.sin(beta * (Math.PI / 180));
-        b = Math.sqrt(c**2 - a**2);
+        b = c * Math.cos(beta * (Math.PI / 180));
     } else if ((type1 === "leg" && type2 === "opposite angle") || (type1 === "opposite angle" && type2 === "leg")) {
         a = num1;
         alpha = num2;
         beta = 90 - alpha;
         c = a / Math.sin(alpha * (Math.PI / 180));
-        b = Math.sqrt(c**2- a**2);
+        b = c * Math.sin(beta * (Math.PI / 180));
     } else if ((type1 === "hypotenuse" && type2 === "angle") || (type1 === "angle" && type2 === "hypotenuse")) {
         c = num1;
         beta = num2;
@@ -42,11 +42,11 @@ function triangle(num1, type1, num2, type2) {
         a = c * Math.sin(alpha * (Math.PI / 180));
         b = c * Math.sin(beta * (Math.PI / 180));
     } else {
-        return "failed";
+        return "failed: unknown combination of side types";
     }
 
-    if (b <= 0 || a >= c || a <= 0 || b >= c ) {
-        return "failed";
+    if (b <= 0 || a >= c || a <= 0 || b >= c) {
+        return "failed: incorrect combination of sides to form a triangle";
     }
 
     console.log(`a = ${a}`);
